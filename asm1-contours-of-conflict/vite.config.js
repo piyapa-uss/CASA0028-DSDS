91874b4ac5169a09aaa6bba498d53a627a789bea
Piyapa S. <piyapa.sotthiwat.25@ucl.ac.uk>
2026-02-23 16:33:54 +0000

    Set base for repo root on GitHub Pages

--- asm1-contours-of-conflict/vite.config.js
+++ asm1-contours-of-conflict/vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: "/CASA0028-DSDS/asm1-contours-of-conflict/",
+  base: "/CASA0028-DSDS/",
   plugins: [react()],
 })
